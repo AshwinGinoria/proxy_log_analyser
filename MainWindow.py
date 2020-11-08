455e01fa77957182a92c5f6f512f627811a72492
Ashwin Ginoria <ashwinginoria@gmail.com>
2020-11-08 18:44:28 +0530

    Added Logging Capabilities

--- MainWindow.py
+++ MainWindow.py
@@ -1,23 +1,32 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from helpers import FindMostLeastFrequent, PlotHistogram, ReadLog, FindCount, PlotHistogram, FindMostLeastFrequent, CountRequest 
+from datetime import datetime
 
 
 class MainWindow(QMainWindow):
     # Initializes Window Geometry and Important Variables
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, verbose = True):
+        self.verbose = verbose
+        self.PrintLog("Initializing Window")
+        
         super(QMainWindow, self).__init__(parent)
-        self.setObjectName("MainWindow")
         self.setWindowTitle("Proxy Log Analyser")
-        self.FilePath = ""
-        self.resize(600, 400)
+        self.resize(400, 225)
+        
+        self.filePath = ""
+        self.logData = None
         self.styleSheet = ""
         with open("design.qss") as qss:
             self.styleSheet = qss.read()
         self.setStyleSheet(self.styleSheet)
+
         self.setupUi()
 
     def setupUi(self):
+        self.PrintLog("Setting-Up User Interface")
+
         self.centralWidget = QWidget(self)
         self.setCentralWidget(self.centralWidget)
 
@@ -28,56 +37,56 @@ class MainWindow(QMainWindow):
         self.filePickerButton = QPushButton("&Choose file", self)
         self.filePickerButton.clicked.connect(self.FilePicker)
 
-        self.refreshFileButton = QPushButton("&Refresh File", self)
-        self.refreshFileButton.clicked.connect(self.RefreshFile)
-
-        # Buttons Arranged Horizontally
-        self.buttonLayout = QHBoxLayout()
-        self.buttonLayout.addWidget(self.filePickerButton)
-        self.buttonLayout.addWidget(self.refreshFileButton)
-
-        # File Content Display
-        self.fileTextViewBox = QTextEdit()
-        self.fileTextViewBox.setReadOnly(True)
+        # Selected File Display
+        self.fileStatusLabel = QLabel(self, text="Status: No log selected")
 
         # Extra Feature Buttons
-        self.plotTimeVsWebCountButton = QPushButton("Show &Time vs Number of Websited")
-        # self.plotTimeVsWebCount.clicked.connect(self.func)
-
-        self.plotTimeVsBlockedWebCountButton = QPushButton(
-            "Show Time vs &Blocked Websites count"
-        )
-        # self.plotTimeVsBlockedWebCountButton.clicked.connect(self.func)
+        self.button1 = QPushButton("Button1")
+        self.button2 = QPushButton("Button2")
+        self.button3 = QPushButton("Button3")
+        self.button4 = QPushButton("Button4")
+        self.button5 = QPushButton("Button5")
+        self.button6 = QPushButton("Button6")
+        self.button7 = QPushButton("Button7")
+        self.button8 = QPushButton("Button8")
 
         # Feature buttons arranged in a grid
         self.featureButtonsLayout = QGridLayout()
-        self.featureButtonsLayout.addWidget(self.plotTimeVsWebCountButton, 0, 0, 1, 1)
-        self.featureButtonsLayout.addWidget(
-            self.plotTimeVsBlockedWebCountButton, 0, 1, 1, 1
-        )
+        self.featureButtonsLayout.addWidget(self.button1, 0, 0, 1, 1)
+        self.featureButtonsLayout.addWidget(self.button2, 0, 1, 1, 1)
+        self.featureButtonsLayout.addWidget(self.button3, 1, 0, 1, 1)
+        self.featureButtonsLayout.addWidget(self.button4, 1, 1, 1, 1)
+        self.featureButtonsLayout.addWidget(self.button5, 2, 0, 1, 1)
+        self.featureButtonsLayout.addWidget(self.button6, 2, 1, 1, 1)
+        self.featureButtonsLayout.addWidget(self.button7, 3, 0, 1, 1)
+        self.featureButtonsLayout.addWidget(self.button8, 3, 1, 1, 1)
+
 
         # Main Vertical Layout
-        self.centralLayout.addLayout(self.buttonLayout)
-        self.centralLayout.addWidget(self.fileTextViewBox)
+        self.centralLayout.addWidget(self.filePickerButton)
+        self.centralLayout.addWidget(self.fileStatusLabel)
         self.centralLayout.addLayout(self.featureButtonsLayout)
-
-    # Refreshes Currently open file for any outside changes made to it
-    def RefreshFile(self):
-        with open(self.FilePath[0], "r") as File:
-            text = File.read()
-            self.FileTextViewBox.setText(text)
 
     # Choose File to perform Operations on
     def FilePicker(self):
-        OldFilePath = self.FilePath
-        self.FilePath = QFileDialog.getOpenFileName(self, "Open log File")
+        oldFilePath = self.filePath
+        self.filePath = QFileDialog.getOpenFileName(self, "Open log File")
 
         # Reverts Changes if No file is Selected or Operation is Cancelled
-        if self.FilePath[0] == "":
-            self.FilePath = OldFilePath
+        if self.filePath[0] == "":
+            self.filePath = oldFilePath
             return
+        
+        fileDate = datetime.strptime(self.filePath.split('-')[-1], '%Y%m%d')
+        
+        self.fileStatusLabel.setText("Status: Loading Log of " + fileDate.strftime('%m/%d/%Y'))
+        
+        self.PrintLog("Loading File")
+        self.logData = ReadLog(self.filePath[0])
+        self.PrintLog("[LOG] File Loaded")
 
-        # Updates FileTextViewBox with new text
-        with open(self.FilePath[0], "r") as File:
-            text = File.read()
-            self.fileTextViewBox.setText(text)
+        self.fileStatusLabel.setText("Status: Log of " + fileDate.strftime('%m/%d/%Y') + "Loaded Successfully")
+
+    def PrintLog(self, entry):
+        if (self.verbose == True):
+            print("[LOG] " + entry)
